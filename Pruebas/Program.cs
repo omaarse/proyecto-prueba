b5d58de7b53fc22835c20af479ae126f7e73ecdc
omaarse <oase_9@hotmail.com>
2020-03-20 15:16:42 -0700

    mi primer commit

---
+++ Pruebas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            Console.WriteLine(Singleton.Instance.msg);
+            Singleton.Instance.msg = "prueba";
+            Console.WriteLine(Singleton.Instance.msg);
+            
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Pruebas/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+        public string msg = "";
+        protected Singleton()
+        {
+            this.msg = "Hola!";
+        }
+
+        public static Singleton Instance
+        {
+
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
