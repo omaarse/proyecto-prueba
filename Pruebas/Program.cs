e10d5a422b678411c817c5b238ac14fe14b2d729
omaarse <oase_9@hotmail.com>
2020-03-21 11:49:27 -0700

    un 3

--- Pruebas/Program.cs
+++ Pruebas/Program.cs
@@ -13,7 +13,7 @@ namespace Pruebas
 
           
             Console.WriteLine(Singleton.Instance.msg);
-            Singleton.Instance.msg = "prueba git hub 2!";
+            Singleton.Instance.msg = "prueba git hub 3!";
             Console.WriteLine(Singleton.Instance.msg);
             
             Console.ReadKey();
